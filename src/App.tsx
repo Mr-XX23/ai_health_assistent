98f0bec35865c094c97b4bf4380fa1f7ad712618
Mr-XX23 <starbalami00@gmail.com>
2025-10-29 10:18:08 +0545

    update dependencies and implement react-router in App component; remove unused auth page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+// local imports
+
+
+const router = createBrowserRouter([
+  
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
