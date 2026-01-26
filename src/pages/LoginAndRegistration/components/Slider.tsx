a07b2e76a077d5b3c6f6eddf1e374d2656543d0a
PIGGE <khadgepranishofficial@gmail.com>
2026-01-26 11:19:35 +0545

    RESPONSIV E PIGGE

--- src/pages/LoginAndRegistration/components/Slider.tsx
+++ src/pages/LoginAndRegistration/components/Slider.tsx
@@ -1,28 +1,28 @@
 import React from "react";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 
 const Slider = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const sidebarItems = [
+    { tag: "Login", path: "/login" },
+    { tag: "Sign Up", path: "/signup" },
+  ];
+
   return (
     <div>
       <div className="flex px-4 py-3">
         <div className="flex h-12 flex-1 items-center justify-center rounded-xl bg-slate-200/80 p-1.5 dark:bg-slate-800">
-          <label className="flex cursor-pointer h-full grow items-center justify-center overflow-hidden rounded-lg px-2 has-[:checked]:bg-background-light has-[:checked]:shadow-md has-[:checked]:text-[#101c22] dark:has-[:checked]:bg-slate-700 dark:has-[:checked]:text-slate-50 text-slate-500 dark:text-slate-400 text-sm font-medium leading-normal transition-all">
-            <span className="truncate">Login</span>
-            {/* <input className="invisible w-0" name="auth-form" type="radio" value="Login"/> */}
-          </label>
-          <label
-            className="flex cursor-pointer h-full grow items-center justify-center overflow-hidden rounded-lg px-2 has-[:checked]:bg-background-light has-[:checked]:shadow-md has-[:checked]:text-[#101c22] dark:has-[:checked]:bg-slate-700 dark:has-[:checked]:text-slate-50 text-slate-500 dark:text-slate-400 text-sm font-medium leading-normal transition-all"
-            onClick={() => navigate("/signup")}
-          >
-            <span className="truncate">Sign Up</span>
-            <input
-              className="invisible w-0"
-              name="auth-form"
-              type="radio"
-              value="Sign Up"
-            />
-          </label>
+          {sidebarItems.map((item) => (
+            <label
+              key={item.path}
+              onClick={() => navigate(item.path)}
+              className="flex cursor-pointer h-full grow items-center justify-center overflow-hidden rounded-lg px-2 has-[:checked]:bg-background-light has-[:checked]:shadow-md text-[#101c22] dark:has-[:checked]:bg-slate-700 dark:has-[:checked]:text-slate-50 text-slate-500 dark:text-slate-400 text-sm font-medium leading-normal transition-all"
+            >
+              <span>{item.tag}</span>
+            </label>
+          ))}
         </div>
       </div>
     </div>
